6fdc1ced8adba8bd38627349b588569dc5089a58
jhashailesh3108 <jhashailesh3108@gmail.com>
2019-04-06 14:57:35 +0530

    Added custom Message handler

--- src/components/user/index.ts
+++ src/components/user/index.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { HTTP401Error } from "../../lib/utils/httpErrors";
 import responseHandler from "../../lib/helpers/responseHandler";
-
+import { user as msg } from "../../lib/helpers/customMessage";
 
 export default [
   {
@@ -9,11 +9,11 @@ export default [
     method: "get",
     handler: async (req: Request, res: Response, next: NextFunction) => {
       try {
-        throw new HTTP401Error("You are not authorized user", "It is discription and it is optional");
+        throw new HTTP401Error();
         const query: number = req.query.numb;
         responseHandler.
         reqRes(req, res).
-        onCreate("created Success Fully", query).
+        onCreate(msg.CREATED, query, msg.CREATED_DEC).
         send();
 
       } catch (e) {
---
+++ src/lib/helpers/customMessage.ts
@@ -0,0 +1,4 @@
+export let user = {
+  CREATED : "Hurray! new user has been created successfully",
+  CREATED_DEC: `This user has been created with the some scenario so adding some description.`
+}
