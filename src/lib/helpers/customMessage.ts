735b7358a55fe359d26e14431fc2d83193eb4375
jhashailesh3108 <jhashailesh3108@gmail.com>
2019-04-06 15:11:04 +0530

    add comments

--- src/lib/helpers/customMessage.ts
+++ src/lib/helpers/customMessage.ts
@@ -1,3 +1,15 @@
+
+/*----------------------------------------------- 
+|| CREATING CUSTOM MESSAGES FOR ALL COMPONENTS 
+------------------------------------------------
+|| Add your all custom messages here right in 
+|| this file. so any thing need to be change 
+|| You will everything at one place
+------------------------------------------------*/
+
+/**
+ * User component custom messages. 
+ */
 export let user = {
   CREATED : "Hurray! new user has been created successfully",
   CREATED_DEC: `This user has been created with the some scenario so adding some description.`
